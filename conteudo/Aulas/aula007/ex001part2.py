44890c2a5ea1cb02b254219734ff81e0b03cb93b
Phablo Souza <phablogm@gmail.com>
2022-02-09 15:16:23 -0300

    commit couse python

---
+++ conteudo/Aulas/aula007/ex001part2.py
@@ -0,0 +1,10 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite mais um valor: '))
+s = n1+n2
+m = n1-n2
+d = n1/n2
+v = n1*n2
+di = n1//n2
+e = n1**n2
+print('A soma é {}, a subtração é {}, e a divisão é {}'.format(s, m, d), end=' ')
+print('A multiplicação é {}, a divisão inteira é {}, a potência é {}'.format(v, di, e))
