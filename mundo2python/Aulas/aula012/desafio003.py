48f5fb92b3e11fa2f287122e98a4944f32830313
Phablo Souza <phablogm@gmail.com>
2022-04-18 07:09:44 -0300

    commit aula 12
    com desafios feitos com auxilio das aulas

--- mundo2python/Aulas/aula012/desafio003.py
+++ mundo2python/Aulas/aula012/desafio003.py
@@ -3,3 +3,11 @@
 # – O segundo valor é maior
 # – Não existe valor maior, os dois são iguais
 
+n1 = int(input('Primeiro número: '))
+n2 = int(input('Segundo número: '))
+if n1 > n2:
+    print('O PRIMEIRO valor é o  maior')
+elif n2 > n1:
+    print('O SEGUNDO valor é maior')
+else:
+     print('Os dois valores são IGUAIS')
