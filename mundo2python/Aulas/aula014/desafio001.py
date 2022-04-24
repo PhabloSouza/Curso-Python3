cda031cd298cc910a7c337e8762f03dae77e80db
Phablo Souza <phablogm@gmail.com>
2022-04-24 19:22:58 -0300

    aula 14
    aula e desafios propostos na aula 14

---
+++ mundo2python/Aulas/aula014/desafio001.py
@@ -0,0 +1,7 @@
+# Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores ‘M’ ou ‘F’. Caso esteja errado, peça a digitação novamente até ter um valor correto.
+# Validação da dados
+
+sexo = str(input('Qual é o sexo: [M/F] ')).strip().upper()[0]
+while sexo not in 'MmFf':
+    sexo = str(input('Dados inválidos. Por favor, informe seu sexo: ')).strip().upper()[0]
+print('Sexo {} registrado com sucesso'.format(sexo))
